90808324a233ff8a5cddfd2371e6634d1d6706c5
Daekhon_jiho-PC\Daekhon_jiho <joe880530@gmail.com>
2017-10-11 14:31:20 +0900

    기본 셋팅

---
+++ FileManager_JH/FileManager_JH/FileManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace FileManager_JH
+{
+    class FileManager
+    {
+        #region 기본값(외부에서 접근 금지)
+        private string str_FileName = "N/A";
+        private string str_FilePath = "N/A";
+        private string str_FileType = ".jpg"; 
+        #endregion
+
+        public string FileName
+        {
+            get
+            {
+                //출력
+                return str_FileName;
+            }
+            set
+            {
+                //입력
+                str_FileName = value;
+            }
+        }
+
+        public string FilePath
+        {
+            get
+            {
+                //출력
+                return str_FilePath;
+            }
+            set
+            {
+                //입력
+                str_FilePath = value;
+            }
+        }
+
+        public string FileType
+        {
+            get
+            {
+                return str_FileType;
+            }
+            set
+            {
+                str_FileType = value;
+            }
+        }
+
+        private void Create()
+        {
+
+
+            MessageBox.Show(FilePath + FileName + FileType);
+            //MessageBox.Show("Create");
+        }
+
+        private void Copy()
+        {
+            MessageBox.Show("Copy");
+        }
+
+        public void Delete()
+        {
+            MessageBox.Show("Delete");
+        }
+
+        public bool Delete_File(string str_Delete_File_Name)
+        {
+            bool return_Value = false;
+
+            FileInfo File = new FileInfo(str_Delete_File_Name);
+
+            if (File != null)
+            {
+                File.Delete();
+            }
+
+            if(File.Exists == true)
+            {
+                return_Value = false;
+            }
+            else
+            {
+                return_Value = true;
+            }
+
+            return return_Value;
+        }
+
+        public void Delete_Folder()
+        {
+            MessageBox.Show("Delete Folder");
+        }
+    }
+}
---
+++ FileManager_JH/FileManager_JH/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManager_JH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            FileManager _FileManager = new FileManager();
+
+            //_FileManager.FilePath = "서울시 서대문구 연희 3동 ";
+            //_FileManager.FileName = "BaBo";
+            //_FileManager.FileType
+
+            //_FileManager.Delete();
+
+            //_FileManager.Delete_File();
+            _FileManager.Delete_File("Wa");
+        }
+    }
+}
